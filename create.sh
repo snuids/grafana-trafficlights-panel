bcecd617df5e1c3a977c0ed467f9d915f2d32a7d
Arnaud Marchand <amarchand@icloud.com>
2024-03-17 21:14:39 +0100

    Save point

--- create.sh
+++ create.sh
@@ -2,17 +2,17 @@
 
 #export GRAFANA_ACCESS_POLICY_TOKEN=TO
 
-version="2.0.6"
+version="2.0.7"
 
 cp README.md ./src/README.md
 
 nvm use v20
 yarn build
 npx @grafana/sign-plugin@latest
-rm -d -r snuids-trafficlights-panel
-cp -r dist/ snuids-trafficlights-panel
+rm -d -r snuids-trafficlights2-panel
+cp -r dist/ snuids-trafficlights2-panel
 
-zip snuids-trafficlights-panel-$version.zip snuids-trafficlights-panel -r
+zip snuids-trafficlights2-panel-$version.zip snuids-trafficlights2-panel -r
 
-rm packages/snuids-trafficlights-panel-$version.zip
-mv snuids-trafficlights-panel-$version.zip ./packages/
+rm packages/snuids-trafficlights2-panel-$version.zip
+mv snuids-trafficlights2-panel-$version.zip ./packages/
